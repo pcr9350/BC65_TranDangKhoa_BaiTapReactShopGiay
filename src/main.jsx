f291a0fdc18de3ba052b46debddd86eb59924fb1
pcr9350 <pcr9350@gmail.com>
2024-04-26 21:46:31 +0700

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import BaiTapGioHang from './BaiTapGioHang/BaiTapGioHang'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import HeaderHome from './components/HeaderHome'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div>
+    {/* <div>
       <BaiTapGioHang />
-    </div>
-  </React.StrictMode>,
-)
+    </div> */}
+    <BrowserRouter>
+      <HeaderHome />
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
